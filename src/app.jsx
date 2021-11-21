c11243d9d704b751a4a1ef9681c68e3138169a48
tnsska2 <vnemftnsska2@naver.com>
2021-11-22 01:10:53 +0900

    Card Maker Init

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import './app.module.css';
+import Login from './components/login/login';
+
+
+function App({ authService }) {
+  return (
+    <>
+      <Login authService={authService} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+
+const Login = ({ authService }) => {
+  return (
+    <section>
+      <Header />
+      <section>
+        <h1>Login</h1>
+        <ul>
+          <li>
+            <button>Google</button>
+          </li>
+          <li>
+            <button>Github</button>
+          </li>
+        </ul>
+      </section>
+      <Footer />
+    </section>
+  );
+}
+
+export default Login;
